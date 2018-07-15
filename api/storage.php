659a9624818f11a9682e73d956b12f49b54a49ca
Lukas <Bone008@users.noreply.github.com>
2018-07-16 00:38:21 +0200

    Back up existing files instead of overwriting them, #22. This is a temporary solution until proper syncing, versioning and possibly undo is implemented.

--- api/storage.php
+++ api/storage.php
@@ -4,6 +4,9 @@ define('STORAGE_DIR', __DIR__ . '/storage/');
 function getFileFromId($id) {
 	return STORAGE_DIR . $id . '.bin';
 }
+function getVersionedFileFromId($id, $version) {
+	return STORAGE_DIR . $id . '__' . $version . '.bin';
+}
 
 Flight::route('GET /storage/@id:[0-9a-f-]+', function($id) {
 	$binFile = getFileFromId($id);
@@ -20,8 +23,25 @@ Flight::route('POST /storage/@id', function($id) {
 	// Note: Not using $_POST because the data is coming in in raw binary form.
 	$postData = file_get_contents('php://input');
 	
-	// TODO: Add versioning/backups of existing files.
-	file_put_contents(getFileFromId($id), $postData);
+	if(empty($postData)) {
+		Flight::json(['error' => 'Received empty data']);
+		return;
+	}
 	
+	$binFile = getFileFromId($id);
+	
+	if(file_exists($binFile)) {
+		$oldVersion = date('Y-m-d-H-i-s', fileatime($binFile));
+		if(!rename($binFile, getVersionedFileFromId($id, $oldVersion))) {
+			Flight::json(['error' => 'Could not back up existing file.']);
+			return;
+		}
+	}
+	
+	$result = file_put_contents($binFile, $postData);
+	if($result === false) {
+		Flight::json(['error' => 'Failed to write to file!']);
+		return;
+	}
 	Flight::json(['success' => true]);
 });
