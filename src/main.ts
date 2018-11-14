fd3fdf0b10d96e91ff5772842309e3dfd13da0f9
Lukas <Bone008@users.noreply.github.com>
2018-11-14 14:46:23 +0100

    Mark dev instance visually more aggressively.

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,14 @@ if (environment.production) {
   enableProdMode();
 } else {
   document.title += " (DEV)";
+
+  // Mark main toolbar in red.
+  const devStyle = document.createElement("style");
+  // WebKit hack
+  devStyle.appendChild(document.createTextNode(""));
+  document.head.appendChild(devStyle);
+  (<any>devStyle.sheet).insertRule(".primary-toolbar { background-color: red !important; }");
+  (<any>devStyle.sheet).insertRule(".app-title:after { content: ' (DEV)'; }");
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
