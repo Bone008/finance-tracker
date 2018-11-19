21127b0b9f430a9dd0a08297fa7dd19ad1431a74
Lukas <Bone008@users.noreply.github.com>
2018-11-19 03:49:28 +0100

    label dominance order: highlight relevant labels and show connections, closes #46.

--- src/app/money/analytics/dialog-label-dominance/dialog-label-dominance.component.ts
+++ src/app/money/analytics/dialog-label-dominance/dialog-label-dominance.component.ts
@@ -1,8 +1,15 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { KeyedSetAggregate } from '../../../core/keyed-aggregate';
 import { DataService } from '../../data.service';
 
 export type LabelDominanceOrder = { [label: string]: number };
+
+export interface LabelCombinationsInfo {
+  label: string;
+  combinations: string[];
+  dominanceValue: number;
+}
 
 @Component({
   selector: 'app-dialog-label-dominance',
@@ -10,30 +17,43 @@ export type LabelDominanceOrder = { [label: string]: number };
   styleUrls: ['./dialog-label-dominance.component.css']
 })
 export class DialogLabelDominanceComponent implements OnInit {
-  readonly allLabels: string[];
-  readonly dominanceOrder: LabelDominanceOrder;
+  readonly labelsWithCombinations: LabelCombinationsInfo[];
+  private readonly dominanceOrder: LabelDominanceOrder;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) data: { dominanceOrder: LabelDominanceOrder },
     dataService: DataService,
     private readonly matDialogRef: MatDialogRef<DialogLabelDominanceComponent>
   ) {
-    this.allLabels = dataService.getAllLabels().sort();
     this.dominanceOrder = data.dominanceOrder;
+
+    // Build matrix of labels that occur together within a single transaction.
+    const labelCombinations = new KeyedSetAggregate<string>();
+    for (const transaction of dataService.getCurrentTransactionList()) {
+      for (let i = 0; i < transaction.labels.length; i++) {
+        // Aggregate for each label all other labels. Note that even adding []
+        // to the aggregate registers the key as an entry, so in the end we get
+        // all labels.
+        labelCombinations.addMany(transaction.labels[i], transaction.labels.slice(0, i));
+        labelCombinations.addMany(transaction.labels[i], transaction.labels.slice(i + 1));
+      }
+    }
+
+    this.labelsWithCombinations = labelCombinations.getEntries().map(entry => ({
+      label: entry[0],
+      combinations: Array.from(entry[1]).sort(),
+      dominanceValue: this.dominanceOrder[entry[0]] || 0,
+    })).sort((a, b) => (a.label < b.label ? -1 : 1));
   }
 
   ngOnInit() {
   }
 
-  getValueForLabel(label: string): number {
-    return this.dominanceOrder[label] || 0;
-  }
-
-  setValueForLabel(label: string, value: number) {
-    this.dominanceOrder[label] = value;
-  }
-
   onSubmit() {
+    // Copy back values from view model into passed in object.
+    for (const labelInfo of this.labelsWithCombinations) {
+      this.dominanceOrder[labelInfo.label] = labelInfo.dominanceValue;
+    }
     this.matDialogRef.close(true);
   }
 }
