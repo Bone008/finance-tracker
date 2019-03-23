b4b2cb4955e09f773b2c51cc9059ef2d3ee3171f
Lukas <Bone008@users.noreply.github.com>
2019-03-23 03:13:09 +0100

    Make syncing data concurrency safe by validating previously loaded data, closes #25.

--- api/storage.php
+++ api/storage.php
@@ -1,5 +1,6 @@
 <?php
 define('STORAGE_DIR', __DIR__ . '/storage/');
+define('HASH_ALGORITHM', 'sha256');
 
 function getFileFromId($id) {
 	return STORAGE_DIR . $id . '.bin';
@@ -12,6 +13,8 @@ Flight::route('GET /storage/@id:[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}'
 	$binFile = getFileFromId($id);
 	
 	if(file_exists($binFile)) {
+		$fileHash = hash_file(HASH_ALGORITHM, $binFile);
+		Flight::etag($fileHash);
 		Flight::response()->header('Content-Type', 'application/octet-stream');
 		readfile($binFile);
 	} else {
@@ -19,8 +22,9 @@ Flight::route('GET /storage/@id:[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}'
 	}
 });
 
-Flight::route('POST /storage/@id:[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}', function($id) {
-	// Note: Not using $_POST because the data is coming in in raw binary form.
+
+function legacyPostStorage($id) {
+	// Note: Not using $_POST because the data is coming in raw binary form.
 	$postData = file_get_contents('php://input');
 	
 	if(empty($postData)) {
@@ -44,4 +48,66 @@ Flight::route('POST /storage/@id:[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}
 		return;
 	}
 	Flight::json(['success' => true]);
+}
+
+Flight::route('POST /storage/@id:[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}', function($id) {
+	$filesData = Flight::request()->files->data;
+	$lastKnownHash = Flight::request()->data->lastKnownHash;
+	
+	if (!isset($filesData['error']) || is_array($filesData['error'])) {
+		// For now, assume it is an old client and fall back to legacy storage.
+		legacyPostStorage($id);
+        //Flight::json(['error' => 'Invalid parameters.']);
+		return;
+    }
+	if(!is_string($lastKnownHash)) {
+        Flight::json(['error' => 'Invalid parameters: Hash missing.']);
+		return;
+	}
+    switch ($filesData['error']) {
+        case UPLOAD_ERR_OK:
+            break;
+        case UPLOAD_ERR_NO_FILE:
+            Flight::json(['error' => 'No file sent.']);
+			return;
+        case UPLOAD_ERR_INI_SIZE:
+        case UPLOAD_ERR_FORM_SIZE:
+            Flight::json(['error' => 'File size limit exceeded.']);
+			return;
+        default:
+            throw new RuntimeException('Unknown upload error.');
+    }
+	
+	$binFile = getFileFromId($id);
+	
+	// Validate the sent hash.
+	$currentHash = null;
+	if(file_exists($binFile)) {
+		$currentHash = hash_file(HASH_ALGORITHM, $binFile);
+		if($lastKnownHash !== $currentHash) {
+            Flight::json(['error' => 'Local database out of date. Please reload.']);
+			return;
+		}
+	}
+	
+	// Create backup of old file.
+	if(file_exists($binFile)) {
+		$oldVersion = date('Y-m-d-H-i-s', filemtime($binFile));
+		if(!rename($binFile, getVersionedFileFromId($id, $oldVersion))) {
+			Flight::json(['error' => 'Could not back up existing file.']);
+			return;
+		}
+	}
+	
+	if(!move_uploaded_file($filesData['tmp_name'], $binFile)) {
+		Flight::json(['error' => 'Failed to accept uploaded file!']);
+		return;
+	}
+	$newHash = hash_file(HASH_ALGORITHM, $binFile);
+	
+	Flight::json([
+		'success' => true,
+		'oldHash' => $currentHash,
+		'newHash' => $newHash
+	]);
 });
--- src/app/money/storage.service.ts
+++ src/app/money/storage.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { gzip, ungzip } from "pako";
 import { Observable, of, throwError } from "rxjs";
-import { catchError, map } from "rxjs/operators";
+import { catchError, mergeMap, switchMap } from "rxjs/operators";
 import { DataContainer } from "../../proto/model";
 import { LoggerService } from "../core/logger.service";
 import { timestampNow } from "../core/proto-util";
@@ -25,6 +25,8 @@ interface SaveStorageResponse extends ApiResponse {
   providedIn: 'root'
 })
 export class StorageService {
+  private lastKnownHash: string | null = null;
+
   constructor(
     private readonly storageSettingsService: StorageSettingsService,
     private readonly httpClient: HttpClient,
@@ -66,10 +68,10 @@ export class StorageService {
         else return throwError(error);
       }))
       .pipe(catchError(e => this.logHttpErrors(e, "Error loading data!")))
-      .pipe(map(responseData => {
+      .pipe(switchMap(responseData => {
         // Pass through not found.
         if (responseData === null) {
-          return new DataContainer();
+          return Promise.resolve(new DataContainer());
         }
 
         // Decompress & decode if we have a response.
@@ -84,16 +86,65 @@ export class StorageService {
         }
 
         try {
-          return DataContainer.decode(uncompressedData);
+          const data = DataContainer.decode(uncompressedData);
+
+          // Upon success, calculate response hash and remember it.
+          return calculateHash(responseData).then(hash => {
+            this.lastKnownHash = hash;
+            return data;
+          });
         } catch (e) {
           this.loggerService.error("Failed to decode:", e);
-          throw "Error decoding loaded data!";
+          return Promise.reject("Error decoding loaded data!");
         }
       }))
       .toPromise();
   }
 
-  loadDataLegacy(): Promise<DataContainer | null> {
+  saveData(data: DataContainer): Promise<void> {
+    data.lastModified = timestampNow();
+
+    const encodedData = DataContainer.encode(data).finish();
+    const compressedData = gzip(encodedData);
+
+    const settings = this.storageSettingsService.getSettings();
+    if (!settings) {
+      return Promise.reject("No data key! Check settings.");
+    }
+
+    const dataBlob = new Blob([compressedData], { type: 'application/octet-stream' });
+    const formData = new FormData();
+    formData.set('data', dataBlob, settings.dataKey);
+    if (this.lastKnownHash !== null) {
+      formData.set('lastKnownHash', this.lastKnownHash);
+    }
+
+    return this.httpClient.post<SaveStorageResponse>(
+      '/api/storage/' + settings.dataKey,
+      formData
+    )
+      .pipe(catchError(e => this.logHttpErrors(e, "Saving failed!")))
+      .pipe(mergeMap<SaveStorageResponse, void>(response => {
+        if (!response.success) {
+          this.loggerService.error("Server error while saving:", response.error);
+          if (response.stackTrace) {
+            this.loggerService.error("Server stack trace:\n" + response.stackTrace);
+          }
+          return Promise.reject(response.error);
+        }
+
+        // On success, update stored hash.
+        return calculateHash(compressedData).then<void>(hash => {
+          this.lastKnownHash = hash;
+        });
+      })).toPromise();
+  }
+
+
+  // Old methods for storing and loading from localStorage.
+  // Code is kept around for when we add local caching for faster loads & offline mode.
+
+  private loadDataLegacy(): Promise<DataContainer | null> {
     return delay(100).then(() => {
       const timeStart = performance.now();
 
@@ -111,45 +162,7 @@ export class StorageService {
     });
   }
 
-  saveData(data: DataContainer): Promise<void> {
-    data.lastModified = timestampNow();
-
-    const encodedData = DataContainer.encode(data).finish();
-    const compressedData = gzip(encodedData);
-
-    // The slicing is necessary because the ArrayBuffer that is underlying
-    // the Uint8Array may have a larger size than the actual data, which messes
-    // up the Content-Length header sent in the request, so the saved file
-    // on the server may contain bogus data.
-    const compactBuffer = compressedData.buffer.slice(0, compressedData.byteLength);
-
-    const settings = this.storageSettingsService.getSettings();
-    if (!settings) {
-      return Promise.reject("No data key! Check settings.");
-    }
-
-    return this.httpClient.post<SaveStorageResponse>(
-      '/api/storage/' + settings.dataKey,
-      compactBuffer,
-      {
-        'headers': {
-          'Content-Type': 'application/octet-stream',
-        }
-      }
-    )
-      .pipe(catchError(e => this.logHttpErrors(e, "Saving failed!")))
-      .pipe(map(response => {
-        if (!response.success) {
-          this.loggerService.error("Server error while saving:", response.error);
-          if (response.stackTrace) {
-            this.loggerService.error("Server stack trace:\n" + response.stackTrace);
-          }
-          throw response.error;
-        }
-      })).toPromise();
-  }
-
-  saveDataLegacy(data: DataContainer): Promise<void> {
+  private saveDataLegacy(data: DataContainer): Promise<void> {
     const error = DataContainer.verify(data);
     if (error) return Promise.reject(error);
 
@@ -163,7 +176,7 @@ export class StorageService {
     }
   }
 
-  deleteData(): Promise<void> {
+  private deleteDataLegacy(): Promise<void> {
     localStorage.removeItem(LEGACY_STORAGE_KEY);
     return Promise.resolve();
   }
@@ -187,4 +200,15 @@ export class StorageService {
     }
     return bufferView;
   }
+}
+
+/** Calculates hash of a binary array and returns it as a hex string. */
+function calculateHash(buffer: Int8Array | Int16Array | Int32Array | Uint8Array | Uint16Array | Uint32Array | Uint8ClampedArray | Float32Array | Float64Array | DataView | ArrayBuffer): PromiseLike<string> {
+  return crypto.subtle.digest('SHA-256', buffer).then(binaryToHex);
+}
+
+function binaryToHex(buffer: ArrayBuffer): string {
+  const byteArray = new Uint8Array(buffer);
+  const hexCodes = [...byteArray].map(value => value.toString(16).padStart(2, '0'));
+  return hexCodes.join('');
 }
