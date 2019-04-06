69610aff5eea3d92192b3ac7ea01548960c63814
Lukas <Bone008@users.noreply.github.com>
2019-04-07 01:44:42 +0200

    Add smart summary of matching transactions while editing a rule.

--- src/app/money/rules/rule-edit/rule-edit.component.ts
+++ src/app/money/rules/rule-edit/rule-edit.component.ts
@@ -1,8 +1,17 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-import { pushDeduplicate, removeByValue } from 'src/app/core/util';
-import { ProcessingAction, ProcessingRule, ProcessingTrigger } from 'src/proto/model';
+import { KeyedNumberAggregate, KeyedSetAggregate } from 'src/app/core/keyed-aggregate';
+import { pluralize, pushDeduplicate, removeByValue } from 'src/app/core/util';
+import { ProcessingAction, ProcessingRule, ProcessingTrigger, Transaction } from 'src/proto/model';
+import { DataService } from '../../data.service';
 import { FilterState } from '../../filter-input/filter-state';
+import { TransactionFilterService } from '../../transaction-filter.service';
+
+/**
+ * In the transaction preview analysis, how many maximum label sets should be displayed,
+ * before showing them as 'various'.
+ */
+const PREVIEW_MAX_LABEL_SETS = 4;
 
 export interface RuleEditConfig {
   rule: ProcessingRule;
@@ -19,15 +28,30 @@ export class RuleEditComponent implements OnInit {
   readonly editMode: 'add' | 'edit';
   readonly filterState = new FilterState();
 
+  /** How many transactions currently pass the given filter. -1 means all. */
+  private matchingTransactions: Transaction[] = [];
+  private isAllMatchingTransactions = true;
+
   constructor(
     @Inject(MAT_DIALOG_DATA) data: RuleEditConfig,
+    private readonly dataService: DataService,
+    private readonly filterService: TransactionFilterService,
     private readonly matDialogRef: MatDialogRef<RuleEditComponent>,
   ) {
     this.rule = data.rule;
     this.editMode = data.editMode;
 
     this.filterState.setValueNow(this.rule.filter);
-    this.filterState.value$.subscribe(value => this.rule.filter = value.trim());
+    this.filterState.value$.subscribe(value => {
+      // Update entity.
+      this.rule.filter = value.trim();
+
+      // Update preview of matching transactions.
+      const txs = this.dataService.getCurrentTransactionList();
+      const filteredTxs = this.filterService.applyFilter(txs, value);
+      this.matchingTransactions = filteredTxs;
+      this.isAllMatchingTransactions = filteredTxs.length === txs.length;
+    });
 
     // Initialize with at least 1 action.
     if (this.rule.actions.length === 0) {
@@ -74,6 +98,86 @@ export class RuleEditComponent implements OnInit {
     else if (type === 'removeLabel') { action.removeLabel = ''; }
     else if (type === 'setField') { action.setField = new ProcessingAction.SetFieldData(); }
     else { console.warn('Unknown action type value: ', type); }
+  }
+
+  /**
+   * Analyzes matching transactions and their label distribution.
+   * Label distributions are reported as one of the following options:
+      - all currently unlabeled
+      - all exactly labeled `foobar`
+      - with label sets {foo, foo bar, <unlabeled>}
+      - with various label sets, but all including `foo`.
+      - with various label sets
+   */
+  getMatchingTransactionsInfo(): { description: string, labelSets: string[][] } {
+    if (this.isAllMatchingTransactions) {
+      return { description: 'all transactions', labelSets: [] };
+    }
+
+    let description = '';
+
+    description += pluralize(this.matchingTransactions.length, 'transaction');
+    description += ', ';
+
+    // For each label, count how many times it appears.
+    const countsByLabel = new KeyedNumberAggregate();
+    let numUnlabeled = 0;
+    for (const tx of this.matchingTransactions) {
+      for (const label of tx.labels) {
+        countsByLabel.add(label, 1);
+      }
+      if (tx.labels.length === 0) {
+        numUnlabeled++;
+      }
+    }
+
+    if (countsByLabel.length === 0) {
+      description += 'all currently unlabeled';
+      return { description, labelSets: [] };
+    }
+
+    const labelsInAll = countsByLabel.getKeys()
+      .filter(label => countsByLabel.get(label) === this.matchingTransactions.length);
+    // Check if all known labels occur in all transactions.
+    if (labelsInAll.length === countsByLabel.length) {
+      description += 'all exactly labeled';
+      return { description, labelSets: [labelsInAll] };
+    }
+
+    // Compute set of each label combination per transaction.
+    const allLabelSets = new KeyedSetAggregate<string>();
+    for (const tx of this.matchingTransactions) {
+      if (tx.labels.length > 0) {
+        const setKey = tx.labels.slice().sort().join(',A,');
+        // Kinda abusing the aggregate mechanic here, we just want each value to be the label set,
+        // so we keep adding the same labels over and over again ...
+        allLabelSets.addMany(setKey, tx.labels);
+      } else {
+        allLabelSets.add('<unlabeled>', '<unlabeled>');
+      }
+    }
+
+    console.log(allLabelSets.getEntries());
+
+    // List label sets explicitly if they are below threshold.
+    if (allLabelSets.length <= PREVIEW_MAX_LABEL_SETS) {
+      description += 'with label sets';
+      return {
+        description,
+        labelSets: allLabelSets.getValues().map(set => Array.from(set).sort())
+      };
+    }
+
+    description += `with ${allLabelSets.length} different label sets`;
+    let labelSets: string[][];
+    if (labelsInAll.length > 0) {
+      description += ', but all including';
+      labelSets = [labelsInAll];
+    } else {
+      labelSets = [];
+    }
+
+    return { description, labelSets };
   }
 
   focusDelayed(focusable: { focus: () => void }) {
