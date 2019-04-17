850ae29837c38941839b430a242a545e92f40084
Lukas <Bone008@users.noreply.github.com>
2019-04-17 15:07:25 +0200

    Notify user about accounts data migration.

--- src/app/money/migrations.service.ts
+++ src/app/money/migrations.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Account, DataContainer, UserSettings } from 'src/proto/model';
+import { KeyedNumberAggregate } from '../core/keyed-aggregate';
 import { extractTransactionData } from './model-util';
 
 /** Processes hooks for preprocessing old migration data. */
@@ -13,6 +14,8 @@ export class MigrationsService {
   constructor() { }
 
   preprocessDataContainer(data: DataContainer) {
+    // Note that this is currently running for empty DataContainers as well (which is probably a
+    // good thing, but we might add default accounts more officially than here).
     this.migrateAccounts(data);
   }
 
@@ -30,6 +33,7 @@ export class MigrationsService {
     }
     data.userSettings.defaultAccountIdOnAdd = ID_CASH;
 
+    const migrationStatistics = new KeyedNumberAggregate();
     // Assign transactions to default accounts depending on isCash flag.
     let foundCreditCard = false;
     for (const txData of extractTransactionData(data.transactions)) {
@@ -40,6 +44,8 @@ export class MigrationsService {
         } else {
           txData.accountId = txData.isCash ? ID_CASH : ID_BANK;
         }
+
+        migrationStatistics.add(txData.accountId.toString(), 1);
       }
     }
 
@@ -53,5 +59,20 @@ export class MigrationsService {
         new Account({ id: ID_CREDIT_CARD, name: "Credit card", icon: "credit_card", currency: "EUR" })
       );
     }
+
+    if (data.transactions.length > 0) {
+      // Notify about migration.
+      let message =
+        `Migrated ${data.transactions.length} transactions to the new account structure.\n` +
+        `The "cash" flag of transactions is now deprecated and should no longer be used.\n\n` +
+        'The following accounts have been set up:\n';
+      for (const account of data.accounts) {
+        message += `${account.name}: ${migrationStatistics.get(account.id.toString())} transactions\n`;
+      }
+
+      console.log(message);
+      alert(message);
+    }
   }
+
 }
