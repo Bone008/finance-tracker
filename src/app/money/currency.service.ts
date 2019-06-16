5a4db823efe34eb1a3efa1969df019d0bd0837e0
Lukas <Bone008@users.noreply.github.com>
2019-06-17 00:42:20 +0200

    Add hard-coded currency conversion API to CurrencyService, closes #125.

--- src/app/money/currency.service.ts
+++ src/app/money/currency.service.ts
@@ -2,17 +2,25 @@ import { Injectable } from '@angular/core';
 import { Money } from 'src/proto/model';
 import { moneyToNumber } from '../core/proto-util';
 
-// TODO: Load from (cached) JSON file.
 const NBSP = '\xA0';
-const SUPPORTED_CURRENCIES = {
-  'EUR': '€',
-  'USD': '$',
-  'CHF': 'Fr',
-  'ILS': '₪',
-  'BGN': 'лв',
-  'RON': 'lei',
-  'HUF': 'Ft',
-  'CZK': 'Kč',
+
+interface CurrencyInfo {
+  symbol: string;
+  exchangeRateToEUR: number;
+}
+type CurrencyInfoMap = { [currencyCode: string]: CurrencyInfo };
+
+// TODO: Load from (cached) JSON file.
+// Hard-coded exchange rates as of 2019-06-16 from www.xe.com.
+const SUPPORTED_CURRENCIES: CurrencyInfoMap = {
+  'EUR': { symbol: '€', exchangeRateToEUR: 1 },
+  'USD': { symbol: '$', exchangeRateToEUR: 0.8924 },
+  'CHF': { symbol: 'Fr', exchangeRateToEUR: 0.8935 },
+  'ILS': { symbol: '₪', exchangeRateToEUR: 0.2478 },
+  'BGN': { symbol: 'лв', exchangeRateToEUR: 0.5113 },
+  'RON': { symbol: 'lei', exchangeRateToEUR: 0.2121 },
+  'HUF': { symbol: 'Ft', exchangeRateToEUR: 0.0031 },
+  'CZK': { symbol: 'Kč', exchangeRateToEUR: 0.0392 },
 };
 
 @Injectable({
@@ -26,18 +34,52 @@ export class CurrencyService {
     return Object.keys(SUPPORTED_CURRENCIES);
   }
 
+  /**
+   * Attempts to convert the given amount in a given currency to a target currency.
+   * Returns null if a currency or its exchange rate is unknown.
+   */
+  convertAmount(amount: number | Money | null | undefined, amountCurrency: string, targetCurrency: string): number | null {
+    if (amount instanceof Money || amount === null || amount === undefined) {
+      amount = moneyToNumber(amount);
+    }
+    // Short-circuit converting to the same currency.
+    if (amountCurrency === targetCurrency) {
+      return amount;
+    }
+
+    const exchangeRate = this.getExchangeRate(amountCurrency, targetCurrency);
+    if (exchangeRate === null) {
+      return null;
+    }
+
+    return amount * exchangeRate;
+  }
+
+  /**
+   * Returns the exchange rate to convert from one currency to another.
+   * Returns null if a currency or its exchange rate is unknown.
+   **/
+  getExchangeRate(fromCurrency: string, toCurrency: string): number | null {
+    const fromInfo = SUPPORTED_CURRENCIES[fromCurrency];
+    const toInfo = SUPPORTED_CURRENCIES[toCurrency];
+    if (!fromInfo || !toInfo) {
+      return null; // Currency not supported.
+    }
+
+    // new_money = old_money * (EUR/old) / (EUR/new)
+    return fromInfo.exchangeRateToEUR / toInfo.exchangeRateToEUR;
+  }
+
   /** Returns the symbol of a currency code, or the code itself if no symbol is known. */
   getSymbol(currencyCode: string): string {
-    const symbol = SUPPORTED_CURRENCIES[currencyCode];
-    return symbol || currencyCode;
+    const info = SUPPORTED_CURRENCIES[currencyCode];
+    return info ? info.symbol : currencyCode;
   }
 
   /** Formats the given amount (number or Money object) with the respective currency symbol. */
   format(amount: number | Money | null | undefined, currencyCode: string, forceSign = false): string {
-    if (amount instanceof Money) {
+    if (amount instanceof Money || amount === null || amount === undefined) {
       amount = moneyToNumber(amount);
-    } else if (amount === null || amount === undefined) {
-      amount = 0;
     }
 
     // Note: not using style:'currency', because it does not allow individual control over the
