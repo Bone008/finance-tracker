5f2df067ecdad45eb2df62db7de8f699d7353be6
Lukas <Bone008@users.noreply.github.com>
2019-09-18 00:19:58 +0200

    Fix split transaction dialog not showing proper currency, closes #137.

--- src/app/money/transactions/dialog-split-transaction/dialog-split-transaction.component.ts
+++ src/app/money/transactions/dialog-split-transaction/dialog-split-transaction.component.ts
@@ -2,6 +2,9 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { TransactionData } from '../../../../proto/model';
 import { moneyToNumber, numberToMoney } from '../../../core/proto-util';
+import { CurrencyService } from '../../currency.service';
+import { DataService } from '../../data.service';
+import { getTransactionDataCurrency } from '../../model-util';
 
 @Component({
   selector: 'app-dialog-split-transaction',
@@ -10,14 +13,20 @@ import { moneyToNumber, numberToMoney } from '../../../core/proto-util';
 })
 export class DialogSplitTransactionComponent implements OnInit {
   splitAmount = 0;
+  readonly currencySymbol: string;
 
   private readonly transactionData: TransactionData;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) data: { transactionData: TransactionData },
+    dataService: DataService,
+    currencyService: CurrencyService,
     private readonly matDialogRef: MatDialogRef<DialogSplitTransactionComponent>
   ) {
     this.transactionData = data.transactionData;
+
+    const currency = getTransactionDataCurrency(this.transactionData, dataService);
+    this.currencySymbol = currencyService.getSymbol(currency);
   }
 
   ngOnInit() {
